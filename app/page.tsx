e5b81763aeac1389d1d237b5dbd644499edbeb29
anuragsharma2512 <anuragsharma4213@gmail.com>
2025-12-28 22:15:38 +0530

    Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import { Button } from "@/components/ui/button";
-import { db } from "@/lib/db";
-import Image from "next/image";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import UserButton from "@/modules/auth/components/user-button";
 
 export default async function Home() {
+  const session = await auth();
+  
+  // Ensure user is authenticated
+  if (!session?.user) {
+    redirect("/auth/sign-in");
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      
       <Button>
         Get Started
       </Button>
+      <UserButton/>
     </div>
     
   );
