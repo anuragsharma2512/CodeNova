74a34f382a16486dce30ca8eb47d2df5a7c26a3d
anuragsharma2512 <anuragsharma4213@gmail.com>
2026-02-02 10:49:50 +0530

    Adding playground file explorer part 1

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths ={
+    REACT:'/stater_files/react-ts',
+    ANGULAR:'/stater_files/angular',
+    NEXTJS:'/stater_files/nextjs',
+    VUE:'/stater_files/vue',
+    EXPRESS:'/stater_files/express-simple',
+    HONO:'/stater_file/hono-nodejs-starter'
+}
